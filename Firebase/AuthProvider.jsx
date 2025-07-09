db21aa1bb6a09961826d6a641692ad721237f60b
IbraHim <mdibrahim752006@gmail.com>
2025-07-10 00:03:03 +0600

    fix navbar login logout with state change & add logout feature

--- Firebase/AuthProvider.jsx
+++ Firebase/AuthProvider.jsx
@@ -40,6 +40,7 @@ const AuthProvider = ({children}) => {
         User,
         UserLogout,
         loading,
+        setUser,
         GoogleLogin
     }
     return (
--- Navbar/Nav.jsx
+++ Navbar/Nav.jsx
@@ -5,14 +5,21 @@ import WebLogo from "../imgs/logo.webp"
 import { Link, NavLink } from 'react-router';
 import { IoLogIn } from "react-icons/io5";
 import useAuth from '../CustomHooks/useAuth';
+import toast from 'react-hot-toast';
 
 const Nav = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const {User} = useAuth()
-
+    const { User , UserLogout, setUser } = useAuth()
+    
     const handleMenuShow = () => {
         setMenuOpen(!menuOpen);
     };
+    const handleUserLogout = () =>{
+        UserLogout().then( ()=>{
+            setUser(null)
+            toast.success("Logout Successfull!")
+        } )
+    }
 
     return (
         <nav className="h-[70px] relative w-full px-6 md:px-16 lg:px-24 xl:px-32 flex items-center justify-between z-20 bg-white text-gray-700 shadow-[0px_4px_25px_0px_#0000000D] transition-all">
@@ -31,16 +38,16 @@ const Nav = () => {
                     <NavLink>All Classes</NavLink>
                     <NavLink>Teach on Skillup</NavLink>
                     {
-                        User ? 
-                        <div className='flex justify-center items-center gap-3'>
+                        User ?
+                            <div className='flex justify-center items-center gap-3'>
                                 <div className="avatar">
                                     <div className="w-10 rounded-full">
-                                        <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                                        <img src={User.photoURL} alt='User Img'/>
                                     </div>
                                 </div>
-                                <button className='btn btn-sm lg:btn-md bg-green-500 text-white'>Logout</button>
-                        </div>
-                        :
+                                <button onClick={handleUserLogout} className='btn btn-sm lg:btn-md bg-green-500 text-white'>Logout</button>
+                            </div>
+                            :
 
                             <div className='md:flex hidden items-center justify-center gap-3'>
                                 <Link to={'/login'} className='flex hover:cursor-pointer justify- items-center gap-2'>
@@ -61,7 +68,7 @@ const Nav = () => {
                 className="menu-btn inline-block md:hidden active:scale-90 transition"
             >
                 {
-                    menuOpen ? <RiMenuUnfold4Fill size={25}/> : <TfiMenu size={25}/>
+                    menuOpen ? <RiMenuUnfold4Fill size={25} /> : <TfiMenu size={25} />
                 }
             </button>
 
@@ -70,19 +77,29 @@ const Nav = () => {
                 id="mobileMen"
                 className={`mobile-menu absolute top-[70px] left-0 w-full bg-white p-6 transition-all duration-300 ease-in-out ${menuOpen ? 'block' : 'hidden'} md:hidden`}
             >
-                <ul className="flex flex-col space-y-4 text-lg">
-                    <li><a href="#" className="text-sm">Home</a></li>
-                    <li><a href="#" className="text-sm">Services</a></li>
-                    <li><a href="#" className="text-sm">Portfolio</a></li>
-                    <li><a href="#" className="text-sm">Pricing</a></li>
-                    <li>
-                      <Link to={'/login'} className='flex hover:cursor-pointer justify-center items-center gap-2'>
-                            <IoLogIn size={25}/><p className='font-semibold'>Login</p>
-                        </Link>
-                    </li>
-                    <li>
-                      <Link to={'/signup'} className='btn  bg-transparent rounded-full px-12 '>Get Started</Link> 
-                    </li>
+                <ul className='flex flex-col justify-start font-semibold xl:text-lg items-center gap-4'>
+                    <NavLink>Home</NavLink>
+                    <NavLink>All Classes</NavLink>
+                    <NavLink>Teach on Skillup</NavLink>
+                    {
+                        User ?
+                            <div className='flex justify-center items-center gap-3'>
+                                <div className="avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src={User.photoURL} alt='User Image'/>
+                                    </div>
+                                </div>
+                                <button onClick={handleUserLogout} className='btn btn-sm lg:btn-md bg-green-500 text-white'>Logout</button>
+                            </div>
+                            :
+
+                            <div className='md:flex hidden items-center justify-center gap-3'>
+                                <Link to={'/login'} className='flex hover:cursor-pointer justify- items-center gap-2'>
+                                    <IoLogIn size={25} /><p className='font-semibold'>Login</p>
+                                </Link>
+                                <Link to={'/signup'} className='btn  bg-transparent rounded-full px-12 '>Get Started</Link>
+                            </div>
+                    }
                 </ul>
 
             </div>
